6cb8aa5edebe7486dcb6b1e61a27236b5611a311
anthony-murè <tony.mure99@gmail.com>
2025-07-08 17:51:29 +0200

    [add]exercise-02

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,30 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+console.log(names);
 
 // Dichiara la funzione qui.
 
+let letterArray =[];
+
+// uso un ciclo for per visualizzare ogni elemento presente nell'array
+// dentro al ciclo recupero la 1 lettera di ogni elemento dell'array
+//generare un nuovo array con le lettere iniziali 
+// fuori dal ciclo usare return per restituire il nuovo arrey di iniziali
+
+function newArray() {
+
+for(let i=0 ;i < names.length ; i++){
+ letter = names[i].charAt(0);
+ letterArray.push(letter);
+}
+
+return letterArray;
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(newArray());
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
