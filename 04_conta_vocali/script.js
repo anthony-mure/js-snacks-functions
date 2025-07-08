ecfb978c8bd30cdb15d323e45aa11903dab6e6f4
anthony-murè <tony.mure99@gmail.com>
2025-07-09 00:33:56 +0200

    [add]exercise-04

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,33 @@
 
 const word = 'javascript';
 
-
 // Dichiara la funzione qui.
 
+//creare una funzione con parametro word 
+//creare una variabile con le vocali
+//creare una variabile per contare le vocali 
+//creare una variabile per mettere le vocali trovate
+//ciclo con for la variabile word per trovare :il numero di vocali contenute e le vocali stesse
+
+function vocalWord(word){
+  const vocals = "aeiou";
+  let numberVocals = 0;
+  let vocalsFound = " ";
+
+  for(let i = 0; i < word.length; i++){
+    if(vocals.includes(word[i])){
+
+      numberVocals++;
+      vocalsFound += word[i];
+    }
+  }
+    return (`${ numberVocals} ${vocalsFound} `);
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+ vocalWord(word);
+ console.log(vocalWord(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
