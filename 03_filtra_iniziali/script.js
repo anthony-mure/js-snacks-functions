ac8258a669ce7f241b3eef0d4c3c0407882e19fb
anthony-murè <tony.mure99@gmail.com>
2025-07-08 20:05:13 +0200

    [add]exercise-03

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,30 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
-
 // Dichiara la funzione qui.
 
+//crare una funzione con 2 parametri
+//ciclo l'array = prendo l'iniziale di ogni parola e la devo confrontare con il paramentro lettera
+// se la parola ha il parametro lettera metto la parola in un nuovo array
+
+let firstLetter = "A";
+let newArrayNames = [];
+
+function newNames (names,firstLetter){
+
+   for(let i = 0; i < names.length ;i++ ){
+     letterNames = names[i].charAt(0);
+
+     if(letterNames === firstLetter){
+        newArrayNames.push(names[i]);
+     }
+   }
+   return newArrayNames;
+
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(newNames(names,firstLetter));
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
